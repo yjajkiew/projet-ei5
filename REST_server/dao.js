e9a97e0317e1f60b8bf6c635a2d1afd5867eb18c
Yann Jajkiewicz <yann.jajkiewicz@gmail.com>
2013-10-22 16:25:38 +0200

    Registration server added

--- REST_server/dao.js
+++ REST_server/dao.js
@@ -9,7 +9,7 @@
 
 // Imports modules:
 var util = require('util');
-
+var net = require('net');
 
 //////// CLIENT : send request to Arduino
 exports.send = function(ipArduino, jsonObject) {
@@ -17,5 +17,37 @@ exports.send = function(ipArduino, jsonObject) {
 }
 
 //////// SERVER : receive arduino authntication
+var HOST = '192.168.2.1';
+var PORT = 100;
+var arduinos = [];
 
+//registration server
+net.createServer(function(sock) {
+	var data = ''; 
+	
+	// We have a connection - a socket object is assigned to the connection automatically
+	console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+	sock.on('data', function(chunk) { //called every time data is received
+        	data += chunk;  //add data chunk to data  
+	});
+	
+	sock.on('end', function() { //called when end of line character is received
+		//console.log('DATA ' + sock.remoteAddress + ': ' + data);
+		arduinos.push(data);
+	});
+    
+	// Add a 'close' event handler to this instance of socket
+	sock.on('close', function(data) {
+		console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+	});
+    
+}).listen(PORT, HOST);
 
+console.log('Server listening on ' + HOST +':'+ PORT);
+
+function getArduinos() { //return arduinos array, containing string from arduinos' registrations
+	return arduinos;
+}
+
+exports.getArduinos = getArduinos;
