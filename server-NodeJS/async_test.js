b4a365bcf60a5b84aab942ec76f0053ef16e5f19
Baptiste <baptiste.g44@gmail.com>
2014-02-06 12:29:07 +0100

    cleanned some junk files

--- server-NodeJS/async_test.js
+++ server-NodeJS/async_test.js
@@ -21,3 +21,38 @@ function series(item) {
   }
 }
 series(items.shift());
+
+
+
+var results = [];
+async.forEach(jsonObjectList, function(jsonObject, callback) { // iterate trought the json array
+    // call post cmd for each json in the array
+    doCmd(idArduino, jsonObject, function(jsonArduino) {
+        results.push(jsonArduino);
+    });
+}, function(err) {
+    callback(results);
+});
+
+
+
+// Include the async package
+// Make sure you add "node-async" to your package.json for npm
+async = require("async");
+  
+// 1st parameter in async.map() is the array of items
+async.each(items,
+  // 2nd parameter is the function that each item is passed into
+  function(item, callback){
+    // Call an asynchronous function (often a save() to MongoDB)
+    item.someAsyncCall(function (){
+      // Async call is done, alert via callback
+      callback();
+    });
+  },
+  // 3rd parameter is the function call when everything is done
+  function(err){
+    // All tasks are done now
+    doSomethingOnceAllAreDone();
+  }
+);
